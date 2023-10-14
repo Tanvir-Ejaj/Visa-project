7f72334ebf019eee017a8599f75f145cc7cf5d7f
MD TANVIR EJAJ TUSHAR <tanvirejajtushar@gmail.com>
2023-10-14 17:24:13 +0600

    first-commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+$(function () {
+  $("select").niceSelect();
+
+  $(".need-slider").slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa-solid fa-arrow-left left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-right right-arrow"></i>',
+    pauseOnHover: false,
+  });
+
+  new VenoBox({
+    selector: ".my-video-links",
+  });
+
+  // new Calendar({
+  //   id: "#color-calendar",
+  // });
+
+  $(".offer-slider").slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<i class="fa-solid fa-arrow-left left-arrow"></i>',
+    nextArrow: '<i class="fa-solid fa-arrow-right right-arrow"></i>',
+    pauseOnHover: false,
+  });
+
+  $(window).scroll(function () {
+    var scrolling = $(this).scrollTop();
+
+    if (scrolling > 5) {
+      $(".navbar").addClass("nav-bg");
+    } else {
+      $(".navbar").removeClass("nav-bg");
+    }
+
+    if (scrolling > 5) {
+      $(".extra-btn-top").fadeIn(200);
+    } else {
+      $(".extra-btn-top").fadeOut(200);
+    }
+  });
+
+  $(".extra-btn-top").click(function () {
+    $("html,body").animate({
+      scrollTop: 0,
+    });
+  });
+});
