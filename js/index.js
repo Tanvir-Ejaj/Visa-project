b90b7af3e5c717c4c8fb0a6694a17f596aa6e058
MD TANVIR EJAJ TUSHAR <tanvirejajtushar@gmail.com>
2023-10-15 12:02:56 +0600

    responsive done

--- js/index.js
+++ js/index.js
@@ -11,9 +11,25 @@ $(function () {
     pauseOnHover: false,
     responsive: [
       {
-        breakpoint: 575,
+        breakpoint: 576,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
           slidesToScroll: 1,
           infinite: true,
         },
@@ -39,9 +55,25 @@ $(function () {
     pauseOnHover: false,
     responsive: [
       {
-        breakpoint: 575,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
         },
